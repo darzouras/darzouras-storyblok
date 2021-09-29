f6f049dcfdd93d58b7afc106b4eb9af99a6b0188
Darlene Zouras <dzouras@gmail.com>
2021-09-29 10:52:59 -0500

    update: add wysiwyg component for text

--- plugins/components.js
+++ plugins/components.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Page from '~/components/Page.vue'
 import Post from '~/components/Post.vue'
 import Header from '~/components/Header.vue'
+import WYSIWYG from '~/components/WYSIWYG.vue'
 
 Vue.component('Page', Page)
 Vue.component('Post', Post)
 Vue.component('Header', Header)
+Vue.component('WYSIWYG', WYSIWYG)
